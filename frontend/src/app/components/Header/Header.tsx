cee0a2dc8d51c4b8443a8813b20c8d5f6197cf5a
Yazhinivenkatesh <yazhinivenkatesh2000@gmail.com>
2023-05-31 17:56:19 +0530

    Added Navbar

--- frontend/src/app/components/Header/Header.tsx
+++ frontend/src/app/components/Header/Header.tsx
@@ -1,9 +1,47 @@
 import "./index.scss";
+import { Logo } from "../../../assets/imageAssets";
+import Icons from "../../../assets/Icons.png";
+import { Link, useLocation } from "react-router-dom";
+import Group from "../../../assets/Group.png";
 
 const Header = () => {
+  const location = useLocation();
+  const walletAddress = "";
+
   return (
-    <div>
-      <h1>This is header</h1>
+    <div className="header">
+      <div className="d-flex align-items-center justify-content-between">
+        <img className="logo"
+          src={Logo}
+          alt="CurveX_Logo"
+        />
+      </div>
+      <div className="nav-items">
+        {
+          location.pathname === "/" ?
+            <>
+              <div>Docs</div>
+              <div>About</div>
+              <div>
+                <Link style={{ textDecoration: 'none' }} to={'/curve-x'}>
+                  <button className="try-btn d-flex">Try the app
+                    <img className="icon"
+                      src={Icons}
+                      alt="wallet-icon"
+                    />
+                  </button>
+                </Link>
+              </div>
+            </>
+            :
+            <button className="connect-btn">
+              <img className="connect-icon"
+                src={Group}
+                alt="wallet-group"
+              />{walletAddress ? walletAddress : 'Connect Wallet'}
+            </button>
+        }
+      </div>
     </div>
   );
 };
