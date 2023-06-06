3e575228e56ba3f6344fe83a9eda8a1e9a041296
Yazhinivenkatesh <yazhinivenkatesh2000@gmail.com>
2023-06-06 11:49:52 +0530

    Changed Asset Names

--- frontend/src/assets/images/imageAssets.ts
+++ frontend/src/assets/images/imageAssets.ts
@@ -1,8 +1,8 @@
 export { default as CurveXLogo } from "../images/MainLogo.png";
 export { default as FooterLogo } from "../images/FooterLogo.svg";
 export { default as WalletIcon } from "../images/WalletIcon.png";
-export { default as Feature1 } from "./feature-1.png";
-export { default as Feature2 } from "./feature-2.png";
-export { default as Feature3 } from "./feature3.png";
-export { default as Feature4 } from "./feature-4.png";
+export { default as Settings } from "./Settings.png";
+export { default as Puzzle } from "./Puzzle.png";
+export { default as Unlock } from "./Unlock.png";
+export { default as Price } from "./Price.png";
 export { default as Greek } from "./Greek.png";
